a09b69822f2b404200fe0f5b82f1b199bf33c064
Richard Zhang <hmzhang@uwaterloo.ca>
2018-01-09 22:04:33 -0500

    setting up camera for MLkit

--- Path Mapper/ViewController.swift
+++ Path Mapper/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    var vidPrevLayer:AVCaptureVideoPreviewLayer!
+    var cameraView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        cameraView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        self.view.addSubview(cameraView)
+        let session = AVCaptureSession()
+        let queue = DispatchQueue(label:"Image Queue")
+        if let cam = AVCaptureDevice.default(for: .video) {
+            session.sessionPreset = .medium
+            cameraView.layer.addSublayer(vidPrevLayer)
+            do{
+                let camInput = try AVCaptureDeviceInput(device: cam)
+                let vidOutput = AVCaptureVideoDataOutput()
+                vidOutput.alwaysDiscardsLateVideoFrames = true
+                vidOutput.setSampleBufferDelegate(self, queue: queue)
+                vidOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+                session.addInput(camInput)
+                session.addOutput(vidOutput)
+                let connection = vidOutput.connection(with: .video)
+                connection?.videoOrientation = .portrait
+                session.startRunning()
+            } catch {
+                print("Camera Error")
+            }
+            
+        } else {
+            print("ERROR: No Video Camera Found.")
+        }
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +48,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
